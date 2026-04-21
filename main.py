70a956e99c8f59f9e00577dec6277834db49771f
alex11091990 <alex11091990@gmail.com>
2026-04-21 14:26:32 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("TOKEN")
-CHAT_ID_1 = os.getenv("CHAT_ID_1")
-CHAT_ID_2 = os.getenv("CHAT_ID_2")
 
 FILE_ID = "AwACAgIAAxkBAAMIaeck7mBixFtnFPvR5iPpFatiMMgAAraPAAIBoRBKIisXN4ENM5g7BA"
 
 
 # =======================
-# /start — личка
+# /start
 # =======================
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет от деда:")
@@ -18,30 +16,12 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 # =======================
-# рассылка в чаты
-# =======================
-async def send_to_chats(context: ContextTypes.DEFAULT_TYPE):
-    try:
-        for chat_id in [CHAT_ID_1, CHAT_ID_2]:
-            await context.bot.send_message(chat_id=chat_id, text="Всех с пятницей!")
-            await context.bot.send_voice(chat_id=chat_id, voice=FILE_ID)
-
-        print("✅ Рассылка отправлена")
-
-    except Exception as e:
-        print("❌ Ошибка рассылки:", e)
-
-
-# =======================
-# запуск
+# запуск бота
 # =======================
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
-
-    # 🔥 каждые 5 минут (для теста)
-    app.job_queue.run_repeating(send_to_chats, interval=300, first=10)
 
     print("🤖 BOT STARTED")
 
