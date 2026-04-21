9d918870b6edaa758b78a5440fa2af2a141f012e
alex11091990 <alex11091990@gmail.com>
2026-04-21 14:20:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,33 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("TOKEN")
+CHAT_ID_1 = os.getenv("CHAT_ID_1")
+CHAT_ID_2 = os.getenv("CHAT_ID_2")
 
-# твой file_id
 FILE_ID = "AwACAgIAAxkBAAMIaeck7mBixFtnFPvR5iPpFatiMMgAAraPAAIBoRBKIisXN4ENM5g7BA"
 
 
 # =======================
-# /start — отправка voice
+# /start — личка
 # =======================
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет от деда:")
+    await update.message.reply_voice(voice=FILE_ID)
+
+
+# =======================
+# рассылка в чаты
+# =======================
+async def send_to_chats(context: ContextTypes.DEFAULT_TYPE):
     try:
-        await update.message.reply_text("Привет от деда:")
-        await update.message.reply_voice(voice=FILE_ID)
+        for chat_id in [CHAT_ID_1, CHAT_ID_2]:
+            await context.bot.send_message(chat_id=chat_id, text="Всех с пятницей!")
+            await context.bot.send_voice(chat_id=chat_id, voice=FILE_ID)
+
+        print("✅ Рассылка отправлена")
 
     except Exception as e:
-        await update.message.reply_text(f"❌ Ошибка: {e}")
-        print("ERROR:", e)
+        print("❌ Ошибка рассылки:", e)
 
 
 # =======================
@@ -28,6 +39,9 @@ def main():
     app = ApplicationBuilder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
+
+    # 🔥 каждые 5 минут (для теста)
+    app.job_queue.run_repeating(send_to_chats, interval=300, first=10)
 
     print("🤖 BOT STARTED")
 
