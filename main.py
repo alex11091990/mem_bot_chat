c4212b1e06f55daefc8cdfd1c4bd0a6eeab7eb09
alex11091990 <alex11091990@gmail.com>
2026-04-21 12:12:51 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import asyncio
 import os
 import requests
 from telegram import Update
@@ -6,34 +5,30 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("TOKEN")
 
-VOICE_URL = "https://drive.google.com/file/d/1bofSKvmZW90mPwPbHSsEsGhbwGaFeYYo/view?usp=sharing"
+VOICE_URL = "https://drive.google.com/uc?export=download&id=1bofSKvmZW90mPwPbHSsEsGhbwGaFeYYo"
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    try:
-        await update.message.reply_text("🎧 Лови голосовое")
+    await update.message.reply_text("🎧 Лови голосовое")
 
-        response = requests.get(VOICE_URL)
+    response = requests.get(VOICE_URL)
 
-        with open("voice.ogg", "wb") as f:
-            f.write(response.content)
+    with open("voice.ogg", "wb") as f:
+        f.write(response.content)
 
-        with open("voice.ogg", "rb") as f:
-            await update.message.reply_voice(voice=f)
-
-        print("✅ Отправлено")
-
-    except Exception as e:
-        print("❌ Ошибка:", e)
+    with open("voice.ogg", "rb") as f:
+        await update.message.reply_voice(voice=f)
 
 
-async def main():
+def main():
     app = ApplicationBuilder().token(TOKEN).build()
+
     app.add_handler(CommandHandler("start", start))
 
-    print("🤖 Бот запущен...")
-    await app.run_polling()
+    print("🤖 BOT STARTED")
+
+    app.run_polling()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
